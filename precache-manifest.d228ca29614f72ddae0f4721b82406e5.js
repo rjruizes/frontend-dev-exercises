3f57382b60c58dbf97e5d1bde463acb8a2ed2442
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2020-08-31 22:50:33 -0400

    Updates

---
+++ precache-manifest.d228ca29614f72ddae0f4721b82406e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd022b41d923476ec86692870c915ec",
+    "url": "/frontend-dev-exercises/index.html"
+  },
+  {
+    "revision": "e54fb35a2208a2fbdc83",
+    "url": "/frontend-dev-exercises/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "aec6524023b8abb273cd",
+    "url": "/frontend-dev-exercises/static/css/main.9f0eb998.chunk.css"
+  },
+  {
+    "revision": "e54fb35a2208a2fbdc83",
+    "url": "/frontend-dev-exercises/static/js/2.aea6ce82.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/frontend-dev-exercises/static/js/2.aea6ce82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec6524023b8abb273cd",
+    "url": "/frontend-dev-exercises/static/js/main.e514078b.chunk.js"
+  },
+  {
+    "revision": "32ea585615221bd18c4d",
+    "url": "/frontend-dev-exercises/static/js/runtime-main.072bc986.js"
+  }
+]);
